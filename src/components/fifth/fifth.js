5d0f45c97b8412ff46ba22016aef73878a8f2a72
Sergey <sergeyballoon@gmail.com>
2017-09-25 00:25:18 +1000

    finish2

--- src/components/fifth/fifth.js
+++ src/components/fifth/fifth.js
@@ -5,6 +5,7 @@ import img_right from './assets/image/img_right.png'
 
 
 import './fifth.css';
+import './common.css';
 
 import Sixth_page from '../sixth/sixth';
 import Second_page from '../second/second';
@@ -26,7 +27,7 @@ class Fifth_page extends Component {
                   <div className = "first-content-container m-b-50">
                     <div className = "row"> 
                       <div className = "row m-l-50 m-r-50"><p className = "font-third-title">Je BMI berekening resultaat</p></div>
-                      <div className = "row m-l-20 m-r-20"><p className = "font-third-second">We hebben al je parameters bekeken en opgenomen in onze berekening.
+                      <div className = "row m-l-20 m-r-20"><p className = "center-font-third-second">We hebben al je parameters bekeken en opgenomen in onze berekening.
                                                 Je BMI IS X. Dit wil zeggen dat je …</p></div>
                     </div>
                     <div className = "row m-t-50 p-l-40 p-r-40">
@@ -49,10 +50,10 @@ class Fifth_page extends Component {
                     </div>
 
                     <div className = "row m-t-70 cutsom-left-flex ">
-                      <div className = "column col-sm-4 ">
+                      <div className = "column1 col-sm-4 ">
                         <img className = "img-fifth" src={img_left}/>
                       </div>
-                      <div className = "column col-sm-8 m-t-20 ">
+                      <div className = "col-sm-8 m-t-20 ">
                         <p className = "font-third-one ">WAAROM IS MIJN BMI BELANGRIJK?</p>
                         <p className = "font-third-second ">e BMI is belangrijk, omdat het aangeeft in hoeverre er sprake is van een
                                                           gezond gewicht. Aan de hand van de formule voor je BMI kun je
@@ -63,12 +64,12 @@ class Fifth_page extends Component {
                     </div>
                     <div className = "row m-t-70 cutsom-right-flex ">
 
-                      <div className = "column col-sm-8 m-t-40">
+                      <div className = "col-sm-8 m-t-40">
                         <p className = "font-third-one ">ANDERE CALCULATORS</p>
                         <p className = "font-third-second ">We bieden nog een reeks van andere calculators aan om je 
                           lichaam nog beter in kaart te brengen. Klik op deze link om terug naar de home pagete gaan.</p>
                       </div>
-                      <div className = "column col-sm-4 ">
+                      <div className = "column1 col-sm-4 ">
                         <img className = "img-fifth" src={img_right}/>
                       </div>
                     </div>
