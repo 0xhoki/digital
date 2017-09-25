36ce55534aa1541b6f07c37fd591cd7a69e8d104
Sergey <sergeyballoon@gmail.com>
2017-09-26 03:03:56 +1000

    update

--- src/components/fifth/fifth.js
+++ src/components/fifth/fifth.js
@@ -26,7 +26,7 @@ class Fifth_page extends Component {
                   <div className = "first-content-container m-b-50">
                     <div className = "row"> 
                       <div className = "row m-l-50 m-r-50"><p className = "font-third-title">Je BMI berekening resultaat</p></div>
-                      <div className = "row m-l-20 m-r-20"><p className = "center-font-third-second">We hebben al je parameters bekeken en opgenomen in onze berekening.
+                      <div className = "row m-l-20 m-r-20"><p className = "center-font-third-second max-width-525">We hebben al je parameters bekeken en opgenomen in onze berekening.
                                                 Je BMI IS X. Dit wil zeggen dat je …</p></div>
                     </div>
                     <div className = "row m-t-50 p-l-40 p-r-40">
@@ -53,8 +53,8 @@ class Fifth_page extends Component {
                         <img className = "img-fifth" src={img_left}/>
                       </div>
                       <div className = "col-sm-8 m-t-20 ">
-                        <p className = "font-third-one ">WAAROM IS MIJN BMI BELANGRIJK?</p>
-                        <p className = "font-third-second ">e BMI is belangrijk, omdat het aangeeft in hoeverre er sprake is van een
+                        <p className = "title-one p-l-50">WAAROM IS MIJN BMI BELANGRIJK?</p>
+                        <p className = "title-two p-l-50">e BMI is belangrijk, omdat het aangeeft in hoeverre er sprake is van een
                                                           gezond gewicht. Aan de hand van de formule voor je BMI kun je
                                                           berekenen of er sprake is van ondergewicht, een gezond gewicht of
                                                           overgewicht. Is je BMI te laag of te hoog? Dan is het verstandig om actie
@@ -64,8 +64,8 @@ class Fifth_page extends Component {
                     <div className = "row m-t-70 cutsom-right-flex ">
 
                       <div className = "col-sm-8 m-t-40">
-                        <p className = "font-third-one ">ANDERE CALCULATORS</p>
-                        <p className = "font-third-second ">We bieden nog een reeks van andere calculators aan om je 
+                        <p className = "title-one ">ANDERE CALCULATORS</p>
+                        <p className = "title-two ">We bieden nog een reeks van andere calculators aan om je 
                           lichaam nog beter in kaart te brengen. Klik op deze link om terug naar de home pagete gaan.</p>
                       </div>
                       <div className = "column1 col-sm-4 ">
